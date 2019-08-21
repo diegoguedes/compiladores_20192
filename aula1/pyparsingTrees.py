b522fd2e5f8f7195c367abf2c86bb5a19d0e6449
Diego Guedes <diegoamericoguedes@gmail.com>
2019-08-21 14:54:16 -0300

    Add files via upload

---
+++ aula1/pyparsingTrees.py
@@ -0,0 +1,7 @@
+import pyparsing
+
+grammar = pyparsing.Forward()
+grammar << pyparsing.Suppress("(") + pyparsing.Word("0123456789") + pyparsing.ZeroOrMore(grammar) + pyparsing.Suppress(")")
+pyparsing.ZeroOrMore(grammar) + pyparsing.Suppress(")")
+query = "(1 (2 (3)) (4 (5 (6) (7) (8))))"
+print(grammar.parseString(query))
